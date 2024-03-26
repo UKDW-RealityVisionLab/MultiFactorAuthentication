ffe0542f99915cee4978775608f6bb901db33eb5
andreasanditya <andreas.anditya@ti.ukdw.ac.id>
2024-03-27 00:30:35 +0700

    api udah jadi tapi lum dipasang penghubung dengan andro dan web, semua method dah bisa

--- Backend/API_Manajemen_User.js
+++ Backend/API_Manajemen_User.js
@@ -9,9 +9,41 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 
+// GET request
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('GET request to the homepage');
 });
+
+// POST request
+app.post('/', (req, res) => {
+  res.send('POST request to the homepage');
+});
+
+// PUT request
+app.put('/', (req, res) => {
+  res.send('PUT request to the homepage');
+});
+
+// PATCH request
+app.patch('/', (req, res) => {
+  res.send('PATCH request to the homepage');
+});
+
+// DELETE request
+app.delete('/', (req, res) => {
+  res.send('DELETE request to the homepage');
+});
+
+// HEAD request
+app.head('/', (req, res) => {
+  res.send('HEAD request to the homepage');
+});
+
+// OPTIONS request
+app.options('/', (req, res) => {
+  res.send('OPTIONS request to the homepage');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
