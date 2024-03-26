33bc981eb1bdd3dafb244598176e16c4544892df
andreasanditya <andreas.anditya@ti.ukdw.ac.id>
2024-03-27 00:21:15 +0700

    api udah jadi tapi lum dipasang penghubung dengan andro dan web

---
+++ Backend/API_Manajemen_User.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
