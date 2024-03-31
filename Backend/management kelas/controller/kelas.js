d45edd4092eabfb63a24d328922cc981abecdf1c
Aryo-Wibisono <yoannes.deo@ti.ukdw.ac.id>
2024-04-01 00:21:52 +0700

    add async because interaction with db

--- Backend/management kelas/controller/kelas.js
+++ Backend/management kelas/controller/kelas.js
@@ -1,94 +1,130 @@
 const { json } = require("body-parser");
 const db = require("../config/db");
+const { query } = require("express");
 
-const formatRes= (status, data, message,res)=>{
+const formatRes = (status, data, message, res) => {
   res.status(status).json({
-    kelas:{
-      status:status,
+    kelas: {
+      status: status,
       dataKelas: data,
-      message:message
-    }
-  })
-}
-
-const getKelas = (req, res) => {
-  const queryGet = "SELECT * FROM kelas"
-  db.query(queryGet, (error,result)=>{
-    try{
-      formatRes(200,result,"berhasil get kelas",res)
-    }catch{
-      res.json({
-        message:"gagal get api kelas"
-      })
-    }
+      message: message,
+    },
   });
 };
 
-const addKelas = (req, res) => {
-  const {kode_kelas, kode_matakuliah, group_kelas, kode_semester, kode_dosen}= req.body
-  const query= `INSERT INTO kelas (kode_kelas, kode_matakuliah, group_kelas, kode_semester, kode_dosen) VALUES ('${kode_kelas}', '${kode_matakuliah}', '${group_kelas}', '${kode_semester}','${kode_dosen}')`;
-
-  db.query(query, (err, result) => {
-    if (err) {
-      res.status(500).json({ message: 'Gagal menambahkan data kelas' });
-      throw err;
-    }
-    formatRes(200,result,"berhasil add kelas",res)
-  });
-};
-
-const editKelas = (req, res) => {
-  const { id } = req.params;
-  console.log("id yang diedit:", id);
-
-  // requestnya
-  const newData = req.body;
-
-  // nampung req dari new data
-  let setValues = '';
-
-  // id ga bole di edit
-  if ('kode_kelas' in newData) {
-    return res.status(400).json({ message: 'Dilarang mengubah ID' });
+const getKelas = async (req, res) => {
+  try {
+    const queryGet = "SELECT * FROM kelas";
+    const result = await new Promise((resolve, reject) => {
+      db.query(queryGet, (error, result) => {
+        if (error) reject(error);
+        resolve(result);
+      });
+    });
+    formatRes(200, result, "berhasil get kelas", res);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Terjadi kesalahan" });
   }
-
-  // Membuat string setValues
-  for (const key in newData) {
-    setValues += `${key}='${newData[key]}', `;
-  }
-
-  // Hapus koma ekstra di akhir string setValues
-  setValues = setValues.slice(0, -2);
-
-  const query = `
-    UPDATE kelas
-    SET ${setValues}
-    WHERE kode_kelas='${id}'
-  `;
-
-  db.query(query, (err, result) => {
-    if (err) {
-      res.status(500).json({ message: 'Gagal mengedit data kelas' });
-      throw err;
-    }
-    formatRes(200,"berhasil edit","berhasil add kelas",res)
-  });
 };
 
+const addKelas = async (req, res) => {
+  try {
+    const {
+      kode_kelas,
+      kode_matakuliah,
+      group_kelas,
+      kode_semester,
+      kode_dosen,
+    } = req.body;
+    const query = `INSERT INTO kelas (kode_kelas, kode_matakuliah, group_kelas, kode_semester, kode_dosen) VALUES (?, ?, ?, ?, ?)`;
+    const values = [
+      kode_kelas,
+      kode_matakuliah,
+      group_kelas,
+      kode_semester,
+      kode_dosen,
+    ];
 
+    const result = await new Promise((resolve, reject) => {
+      db.query(query, values, (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      });
+    });
 
-const deleteKelas = (req, res) => {
-  const { id } = req.params;
+    formatRes(200, result, "berhasil add kelas", res);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Terjadi kesalahan" });
+  }
+};
+
+const deleteKelas = async (req, res) => {
+  try{
+    const { id } = req.params;
   console.log("id yang dihapus:", id);
 
-  const query= `DELETE FROM kelas WHERE kode_kelas= '${id}';`
-  
-  db.query(query, (err, result) => {
-    if (err) {
-      res.status(500).json({ message: 'Gagal menghapus data kelas' });
-      throw err;
-    }
-    formatRes(200,id,"berhasil hapus kelas",res)
+  const query = `DELETE FROM kelas WHERE kode_kelas= '${id}';`;
+
+  const result = await new Promise((resolve, reject)=>{
+    db.query(query,(error, result)=>{
+      if(error) reject(error);
+      resolve(result);
+    });
   });
+  formatRes(200,id,"berhasil hapus kelas",res)
+  }
+  catch(error){
+    console.error(error);
+    return res.status(500).json({ message: "Terjadi kesalahan" });
+  }
 };
+
+
+const editKelas = async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log("id yang diedit:", id);
+
+    // requestnya
+    const newData = req.body;
+
+    // nampung req dari new data
+    let setValues = "";
+
+    // id ga bole di edit
+    if ("kode_kelas" in newData) {
+      return res.status(400).json({ message: "Dilarang mengubah ID" });
+    }
+
+    // Membuat string setValues
+    for (const key in newData) {
+      setValues += `${key}='${newData[key]}', `;
+    }
+
+    // Hapus koma ekstra di akhir string setValues
+    setValues = setValues.slice(0, -2);
+
+    const query = `
+      UPDATE kelas
+      SET ${setValues}
+      WHERE kode_kelas='${id}'
+    `;
+
+    await new Promise((resolve, reject) => {
+      db.query(query, (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      });
+    });
+
+    formatRes(200, "berhasil edit", "id"+id, res);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Terjadi kesalahan' });
+  }
+};
+
+
 module.exports = { getKelas, addKelas, editKelas, deleteKelas };
