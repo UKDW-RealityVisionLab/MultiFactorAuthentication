4030d9e15c4c44473b2c2f8506d32825b7cda9f1
dendy prtha <prtha.dendy@gmail.com>
2024-04-03 09:46:05 +0700

    refactor code & project structure add pinia for state management add vue router add dummy backend

---
+++ FrontendWeb/src/views/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div v-if="user">
+        <h1>Hi Dummy!</h1>
+        <p>You're logged in with Vue 3 + Pinia & JWT!!</p>
+        <p><router-link to="/users">Manage Users</router-link></p>
+    </div>
+</template>
