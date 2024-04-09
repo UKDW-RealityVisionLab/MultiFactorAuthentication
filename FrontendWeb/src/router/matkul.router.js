94c8688fcc87ce94cb3a67f94e52a7e93f720253
Aryo-Wibisono <yoannes.deo@ti.ukdw.ac.id>
2024-04-09 15:04:25 +0700

    add  layout and navigate to matkul page

---
+++ FrontendWeb/src/router/matkul.router.js
@@ -0,0 +1,11 @@
+import { Layout, List } from '@/views/matkul';
+
+export default {
+    path: '/mataKuliah',
+    component: Layout,
+    children: [
+        { path: '', component: List },
+        // { path: 'add', component: AddEdit },
+        // { path: 'edit/:id', component: AddEdit }
+    ]
+};
