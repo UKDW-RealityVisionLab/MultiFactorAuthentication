b24e6145bc4bd6c9c7269997ddbe42a9b1d0d7ab
Dedeo3 <yoannes.deo@ti.ukdw.ac.id>
2024-04-24 11:07:55 +0700

    CRUD BE JADWAL CLEAR

---
+++ Backend/routes/jadwal.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const route = express.Router();
+
+const controller= require('../controller/jadwal')
+
+route.get('/', controller.getJadwal)
+route.post('/', controller.addJadwal)
+
+route.patch('/:kode_jadwal',controller.editJadwal)
+
+route.delete('/:kode_jadwal',controller.deleteJadwal)
+route.get('/:kode_jadwal',controller.getByKodeJadwal)
+
+module.exports= route
