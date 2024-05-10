28c3f3f3c23c546e19b327fc4779029a9fc5c5d6
Dedeo3 <yoannes.deo@ti.ukdw.ac.id>
2024-05-10 22:01:17 +0700

    success validation time on qr

--- Backend/controller/qr.js
+++ Backend/controller/qr.js
@@ -8,33 +8,44 @@ const formatRes = (status, data, message, res) => {
     });
 };
 
+
 const QRCode = require('qrcode');
+let currentTime = new Date();
+let expiryTime = new Date(currentTime.getTime() + 0.5 * 60000); // Calculate expiry time
 
 const generateQr = (req, res) => {
     const { kode_jadwal } = req.params;
-    
-    const data = kode_jadwal;
-    const timestamp = Date.now(); 
-    const expiryTime = timestamp + (5 * 60 * 1000); // Menambah 5 menit ke timestamp
+    const created_at = currentTime.toLocaleString(); 
+    const expired_at = expiryTime.toLocaleString();
 
-    const qrData = { text: data, expiry: expiryTime }; 
-    QRCode.toDataURL(data, (err, url) => {
+    console.log('generate:', created_at);
+    console.log('expire:', expired_at);
+
+    const data = [
+        { data: `${kode_jadwal} ${created_at}` },
+        { data: `${kode_jadwal} ${expired_at}` }
+    ];
+
+    QRCode.toDataURL(data, { scale: 10 }, (err, url) => {
+        console.log('data generate is:', data);
         if (err) {
             console.error(err);
             formatRes(500, null, 'Error generating QR code', res);
         } else {
             formatRes(200, url, 'QR code generated successfully', res);
         }
-        if(req.body > expiryTime){
-            formatRes(403, null,"waktu telah berakhir",res)
-        }
-        
     });
-
-
-   
-
 };
 
 
-module.exports = { generateQr };
+const validation = (req, res) => {
+    const timeReceive = new Date().toLocaleString();
+
+    if (timeReceive > expiryTime.toLocaleString()) {
+        formatRes(400, null, 'QR code is not valid - QR code expired', res);
+    } else {
+        formatRes(200," GENERATE: " + currentTime.toLocaleString()+ "REQ RECEIVED AT: "+ timeReceive  +"EXPIRED:"+ expiryTime.toLocaleString(), 'QR code is valid', res);
+    }
+}
+
+module.exports = { generateQr, validation };
--- Backend/routes/qr.js
+++ Backend/routes/qr.js
@@ -4,6 +4,7 @@ const route = express.Router();
 
 const controller= require('../controller/qr')
 
+route.post('/:kode_jadwal',controller.validation)
 route.get('/:kode_jadwal', controller.generateQr)
 
 module.exports= route
