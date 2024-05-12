950ef51b658d99698bc088d7def6c06f124f0910
Dedeo3 <yoannes.deo@ti.ukdw.ac.id>
2024-05-12 20:59:49 +0700

    Update qr.js

--- Backend/controller/qr.js
+++ Backend/controller/qr.js
@@ -16,7 +16,7 @@ let expiryTime = new Date(currentTime.getTime() + 5 * 60000); // Calculate expir
 var dataQr;
 const generateQr = (req, res) => {
     const { kode_jadwal } = req.params;
-    const created_at = currentTime.toLocaleString(); 
+    const created_at = currentTime.toLocaleString();
     const expired_at = expiryTime.toLocaleString();
 
     console.log('generate:', created_at);
@@ -34,7 +34,7 @@ const generateQr = (req, res) => {
             formatRes(500, null, 'Error generating QR code', res);
         } else {
             formatRes(200, url, 'QR code generated successfully', res);
-            dataQr=data
+            dataQr = data
             return dataQr
             // data(dataQr)
         }
@@ -42,27 +42,45 @@ const generateQr = (req, res) => {
 };
 
 
-const validation = (req, res) => {
+const validation = async (req, res) => {
     const timeReceive = new Date().toLocaleString();
     //cek jika ga req empty
-    if (!req.body) {
+    if (!req.body || req.body == null) {
         formatRes(400, null, 'Request body is missing or empty', res);
         return;
     }
 
-    let dataReceived = JSON.stringify(req.body[0].data+req.body[1].data);
-    let dataQrValid = JSON.stringify(dataQr[0].data+dataQr[1].data);
-    
+    let dataReceived = JSON.stringify(req.body[0].data + req.body[1].data);
+    let dataQrValid = JSON.stringify(dataQr[0].data + dataQr[1].data);
+
 
     if (timeReceive > expiryTime.toLocaleString()) {
         formatRes(400, null, 'QR code is not valid - QR code expired', res);
     } else if (dataReceived !== dataQrValid) {
         formatRes(400, "Data received: " + dataReceived + " valid data: " + dataQrValid, "QR IS NOT MATCH", res);
     } else {
-        formatRes(200, " GENERATE: " + currentTime.toLocaleString() + " REQ RECEIVED AT: " + timeReceive  + " EXPIRED:" + expiryTime.toLocaleString() + "data req: " + dataReceived, 'QR code is valid', res);
-        
-    }
-    // console.log(dataQrValid[0].data)
+        formatRes(200, " GENERATE: " + currentTime.toLocaleString() + " REQ RECEIVED AT: " + timeReceive + " EXPIRED:" + expiryTime.toLocaleString() + "data req: " + dataReceived, 'QR code is valid', res);
+    //     try {
+    //         const user = dataReceived[2].user; // Extract the ID from the request parameters
+    //         const queryUpdate = `UPDATE presensi SET hadir = hadir WHERE nama = '${user}'`;
+    //         const result = await new Promise((resolve, reject) => {
+    //             db.run(queryUpdate, [user], function (error) {
+    //                 if (error) reject(error);
+    //                 resolve({ affectedRows: this.changes }); // Return the number of affected rows
+    //             });
+    //         });
+
+    //         if (result.affectedRows > 0) {
+    //             formatRes(200, result, "Data berhasil diupdate", res);
+    //         } else {
+    //             return res.status(404).json({ message: "Data tidak ditemukan" });
+    //         }
+    //     } catch (error) {
+    //         console.error(error);
+    //         return res.status(500).json({ message: "Terjadi kesalahan" });
+    //     }
+    // }
+}
 }
 
 module.exports = { generateQr, validation };
