45771d7ee364e150e1d9122d001fc14b219438ae
Aryo-Wibisono <yoannes.deo@ti.ukdw.ac.id>
2024-07-28 00:32:27 +0700

    Success verify idJadwal

--- Backend/controller/qr.js
+++ Backend/controller/qr.js
@@ -1,9 +1,8 @@
-const db = require("../config/db");
 const QRCode = require('qrcode');
 let currentTime = new Date();
 let expiryTime = new Date(currentTime.getTime() + 5 * 60000); // Calculate expiry time
 let dataQr;
-const crypto = require('crypto');
+let kodeJadwal;
 
 const generateQr = (req, res) => {
 
@@ -24,11 +23,6 @@ const generateQr = (req, res) => {
 
         console.log('generate:', created_at);
         console.log('expire:', expired_at);
-
-        // const data = {
-        //     qrCodeData: `${kode_jadwal} ${created_at}`,
-        //     kodeJadwal: `${kode_jadwal}`
-        // }
         const data= `${kode_jadwal} ${created_at}`
 
         QRCode.toDataURL(data, { scale: 8 }, (err, url) => {
@@ -39,6 +33,8 @@ const generateQr = (req, res) => {
             } else {
                 // Save data and send response indicating QR code generated successfully
                 dataQr = data;
+                kodeJadwal= kode_jadwal;
+                console.log("generate kode jadwal:", kodeJadwal)
                 generateQr.QRCode = url;
                 res.json(url);
             }
@@ -59,24 +55,23 @@ const validation = async (req, res) => {
     let receivedKodeJadwal;
     
     try {
-        dataReceived = req.body.qrCodeData + req.body.kode_jadwal;
+        dataReceived = req.body.qrCodeData + req.body.idJadwal;
         dataQrReceive = req.body.qrCodeData;
-        receivedKodeJadwal = req.body.kodeJadwal;
+        receivedKodeJadwal = req.body.idJadwal;
     } catch (error) {
         res.status(400).json({ message: 'Invalid request body structure' });
         return;
     }
 
-    console.log(`validasi dataQr data: ${dataQrReceive}`);
-    console.log(`validasi kode jadwal data: ${receivedKodeJadwal}`);
-
     if (timeReceive > expiryTime.toLocaleString()) {
         res.status(500).json("QR is expired");
-    } else if (dataQrReceive !== dataQr) {
+    } else if (dataQrReceive !== dataQr || receivedKodeJadwal != kodeJadwal ) {
         res.status(500).json('QR is not matching');
+        console.log(`validasi kode jadwal yang diterima: ${receivedKodeJadwal}`);
+        console.log("validation kode jadwal:", kodeJadwal)
+        console.log(`validasi dataQr data yang diterima: ${dataQrReceive}`);
+        console.log(`validasi dataQR: ${dataQr}`);
     } else {
-        // Valid QR code, continue processing
-        // await presensIfvalid(req, res);
         res.json({ qrCodeData: "verify qrcode berhasil" });
     }
 };
